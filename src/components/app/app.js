6e8da16f3d47021e0708a1e3e3173082a04fa1ab
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2026-05-04 15:58:15 +0300

    onToggleRise in App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -55,13 +55,23 @@ class App extends Component {
   };
 
   onToggleRise = (id) => {
-    console.log(`Rise ${id}`);
+    this.setState(({data}) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return {...item, rise: !item.rise};
+        }
+        return item;
+      }),
+    }));
   };
 
   render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter((item) => item.increase).length;
+
     return (
       <div className="app">
-        <AppInfo />
+        <AppInfo employees={employees} increased={increased} />
 
         <div className="search-panel">
           <SearchPanel />
