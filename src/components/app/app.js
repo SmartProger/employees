e4ebb24d949f55b289cc75cf064c318447eb9e5f
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2026-05-10 18:08:02 +0300

    filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,6 +18,7 @@ class App extends Component {
         {name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3},
       ],
       term: "",
+      filter: "all",
     };
     this.maxId = 4;
   }
@@ -68,11 +69,26 @@ class App extends Component {
     this.setState({term});
   };
 
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case "rise":
+        return items.filter((item) => item.rise);
+      case "moreThan1000":
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({filter});
+  };
+
   render() {
-    const {data, term} = this.state;
+    const {data, term, filter} = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
-    const visibleData = this.searchEmp(data, term);
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
     return (
       <div className="app">
@@ -80,7 +96,7 @@ class App extends Component {
 
         <div className="search-panel">
           <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-          <AppFilter />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
 
         <EmployeesList
